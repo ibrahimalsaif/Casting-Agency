50322fdb3b26f88c0bdcc96372b231b3eb1b5057
ibrahim <62214241+ibrahimalsaif@users.noreply.github.com>
2020-08-08 16:59:46 +0300

    fix an error

--- database/models.py
+++ database/models.py
@@ -3,9 +3,8 @@ from sqlalchemy import Column, String, Integer, Date, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-# database_name = "agency"
-# database_path = "postgres://{}/{}".format('postgres:7654321@localhost:5432', database_name)
-database_path = os.environ['DATABASE_URL']
+database_name = "agency"
+database_path = "postgres://{}/{}".format('postgres:7654321@localhost:5432', database_name)
 
 db = SQLAlchemy()
 
